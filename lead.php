25956a9c2f817c81c46fbbbb7864ec4f10a09932
alexskursky <49185626+alexskursky@users.noreply.github.com>
2020-05-13 14:27:26 +0300

    Add files via upload

---
+++ lead.php
@@ -0,0 +1,82 @@
+<html lang="en"><head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+  <meta name="HandheldFriendly" content="true">
+  <title>Thank You</title>
+  <script type="text/javascript" src="https://submit.jotformpro.com/js/prototype.js"></script>
+  <style>
+    body {
+      font-size:12px;
+      font-family:Verdana;
+    }
+  </style>
+</head>
+<body style="padding:24px;">
+  <p>&nbsp;</p>
+<div style="text-align:center;">
+<h1><span style="font-size:x-large;color:#0000ff;"><img style="vertical-align:middle;" src="http://cms.interlogy.com/uploads/image_upload/image_upload/global/12104_Smiley-company-logo_low_reasonably_small.jpg" alt="" width="50" height="50">&nbsp;Thank You sa!</span></h1>
+</div>
+<div style="text-align:center;"><span style="color:#008000;"><strong>Your submission has been received. We'll get back to you shortly!</strong></span></div>
+<table border="0" cellspacing="0" cellpadding="0" width="100%" bgcolor="#ffffff">
+<tbody>
+<tr>
+<td height="30">&nbsp;</td>
+</tr>
+<tr>
+<td align="center">
+<table border="0" cellspacing="0" cellpadding="0" width="600" bgcolor="#eeeeee">
+<tbody>
+<tr>
+<td width="13" height="30" style="background-image:url(&quot;http://www.jotform.me/images/win2_title_left.gif&quot;);">&nbsp;</td>
+<td align="left" valign="bottom" style="background-image:url(&quot;http://www.jotform.me/images/win2_title.gif&quot;);"><strong>Submissions</strong></td>
+<td width="14" style="background-image:url(&quot;http://www.jotform.me/images/win2_title_right.gif&quot;);">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<table border="0" cellspacing="0" cellpadding="0" width="600">
+<tbody>
+<tr>
+<td width="4" style="background-image:url(&quot;http://www.jotform.me/images/win2_left.gif&quot;);">&nbsp;</td>
+<td align="center" bgcolor="#FFFFFF">
+<table border="0" cellspacing="0" cellpadding="5" width="100%">
+<tbody>
+<tr>
+<td style="text-decoration:underline;padding:5px;" width="170" bgcolor="#f9f9f9"><strong>Question</strong></td>
+<td style="text-decoration:underline;padding:5px;" bgcolor="#f9f9f9"><strong>Answer</strong></td>
+</tr>
+<tr>
+<td style="padding:5px;" width="170" bgcolor="#FFFFFF">Name</td>
+<td style="padding:5px;" bgcolor="#FFFFFF">sa</td>
+</tr>
+</tbody>
+</table>
+</td>
+<td width="4" style="background-image:url(&quot;http://www.jotform.me/images/win2_right.gif&quot;);">&nbsp;</td>
+</tr>
+<tr>
+<td style="background-image:url(&quot;http://www.jotform.me/images/win2_foot_left.gif&quot;);font-size:4px;" height="4">&nbsp;</td>
+<td style="background-image:url(&quot;http://www.jotform.me/images/win2_foot.gif&quot;);font-size:4px;">&nbsp;</td>
+<td style="background-image:url(&quot;http://www.jotform.me/images/win2_foot_right.gif&quot;);font-size:4px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td height="30">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p><br><br></p>
+  <script type="text/javascript">
+    if (window.parent !== window) { 
+        var margin = parseInt($$('body')[0].getStyle('margin-top')) + parseInt($$('body')[0].getStyle('margin-bottom'));
+        var height = parseInt($$('body')[0].getHeight())+margin;
+        window.parent.postMessage('setHeight:' + height, '*');
+        window.parent.postMessage({ action: 'submission-completed' }, '*');
+    }
+  </script>
+
+
+</body></html>
